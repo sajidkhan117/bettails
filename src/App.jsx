f1a30a4b0111e41b11afc427358c7e4d647be63a
sajidkhan117 <sajidahsan230@gmail.com>
2021-12-28 20:43:34 +0500

    Changes have been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
 import "./App.css";
+import Login from "./Login.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <Login/>
     </div>
   );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,63 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import { useForm } from "react-hook-form";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faEye } from "@fortawesome/free-solid-svg-icons";
-// const eye = <FontAwesomeIcon icon={faEye} />;
+import React, { useState } from "react";
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
 
 import "./Login.css";
+// import { useState } from 'react';
 const login = () => {
-  // const { register, handleSubmit } = useForm();
-  // const onSubmit = data => {
-  //   console.log(data);
-  // };
+  const [show, setShow] = useState(false);
+  const toggleBtn = () =>{
+      setShow(prevState => !prevState)
+  }
   return (
     
     <div className="LoginForm">
-    <div>
-
-    </div>
       <div className="Topbar">
         <h1>betfair</h1>
       </div>
       <form action="" className="Form">
-        <h3>Please Login below</h3>
-        <div className="FormInputs">
-          <div className="FormData">
-            <label htmlFor="Email or userName"> Email or userName </label>
-            <input
-              type="text"
-              autoComplete="off"
-              name="username"
-              id="username"
-              // ref={register({ required: "This is required." })}
-            ></input>
-          </div>
+        <fieldset className="fieldset">
+          <h3>Please Login below</h3>
+          <div className="FormInputs">
+            <div className="FormData">
+              <label htmlFor="Email or userName"> Email or userName </label>
+              <input
+                type="text"
+                autoComplete="off"
+                name="username"
+                id="username"
+               
+              ></input>
+            </div>
 
-          <div className="FormDataPassword">
-            <label htmlFor="Password"> Password </label>
-            <input
-              type="password"
-              autoComplete="off"
-              name="password"
-              id="Password"
-              // ref={register({ required: "This is required." })}
-            ></input>
-            {/* <i>{eye}</i> */}
+            <div className="FormDataPassword">
+              <label htmlFor="Password"> Password </label>
+              <input
+                // type="password"
+                type={show ? "text" : "password"}
+                // autoComplete="off"
+                // name="password"
+                // id="Password"
+              ></input>
+              <button className="icons" onClick={toggleBtn}>
+              {show ? <AiOutlineEyeInvisible/> :
+                <AiOutlineEye /> 
+              }
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="Btn">
-          <button type="submit">Login</button>
-          <p>forget your <a>username</a> or <a>Password</a></p>
-        </div>
+          <div className="Btn">
+            <button type="submit">Login</button>
+            <p>
+              forget your <a>username</a> or <a>Password</a>
+            </p>
+          </div>
+        </fieldset>
       </form>
 
       <div className="secondPortion">
+        <div className="secondPara">
           <h3>New to Betfair</h3>
           <div className="imagePortion"></div>
-            <div className="secondBtn">
-              <button>Join Now</button>
-            </div>
+          <div className="secondBtn">
+            <button>Join Now</button>
+          </div>
+        </div>
       </div>
     </div>
   );
